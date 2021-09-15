b153403cf7f903d6c344d109b2a281de6341ac89
Tata <tatarusetskaya@gmail.com>
2021-09-15 21:56:29 +0300

    Update task1.js

--- sprint02/task1.js
+++ sprint02/task1.js
@@ -1,6 +1,12 @@
-const filterNums = (arr, n, p) => {
-    n = n || 0;
-    if (p === 'greater' || typeof p === 'undefined') return (arr.filter((item) => item > n));
-    if (p === 'less') return (arr.filter((item) => item < n));
-    if (typeof p === 'undefined' && typeof n === 'undefined') return (arr.filter((item) => item > 0));
+// The function filterByN receives an array of integers, a number and a parameter (greater, less). Print a new array, where all elements will be greater/less than this number
+// By default, the number is 0, the parameter is greater.
+// Example:
+// filterNums([-1, 2, 4, 0, 55, -12, 3], 11, 'greater');  //[ 55]
+// filterNums([-2, 2, 3, 0, 43, -13, 6], 6, 'less'); // [-2, 2, 3, 0, -13]
+// filterNums([-2, 2, 3, 0, 43, -13, 6], -33, 'less'); //  []
+// filterNums([-2, 2, 3, 0, 43, -13, 6]); // [2, 3, 43, 6]
+// filterNums([-2, 2, 3, 0, 43, -13, 6], 23);  // [43]
+
+const filterNums = (arr, n = 0, how = 'greater') => {
+    return arr.filter(el => how === 'less' ? el < n : el > n);
 };
