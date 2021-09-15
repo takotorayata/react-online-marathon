9a9e5097210cc6a16db7cfb65f7b702e07b4a17a
Tata <tatarusetskaya@gmail.com>
2021-09-15 22:11:41 +0300

    Update task6.js

--- sprint01/task6.js
+++ sprint01/task6.js
@@ -10,3 +10,15 @@ function accountPatients(n) {
     }
     return [admit, discharge];
 }
+
+
+// Please, implement a function accountPatients that takes a count of free beds in a hospital and returns two functions:
+// the first one for adding a patient
+// the second one for discharging a patient
+// accountPatients should keep track of free beds in a hospital and every time when a patient is admitted or discharged, print the count to the console like in examples:
+// A patient was discharged, 54 beds are available
+// A patient was admitted, 34 beds are available
+// When there are no beds available, 
+// Can not admit a patient, no beds available should be printed
+// When there is an attempt to discharge a patient when there are no patients,
+// There are no patients to discharge should be printed
