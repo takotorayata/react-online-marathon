b8d390979df2ad3f8145ad9e396b4a11c78e4b40
Tata <tatarusetskaya@gmail.com>
2021-09-15 21:50:35 +0300

    Create task2.js

---
+++ sprint03/task2.js
@@ -0,0 +1,7 @@
+const product = function() {
+    return [].reduce.call(arguments, function(res, elem) {
+      return res * elem;
+    }, this.product);
+};
+const contextObj = {product: 10};
+const getProduct = product.bind(contextObj, 2, 3);
