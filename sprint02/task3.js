8b1cf17e5d2b7237ac4f0d76686b189a8ec2bd19
Tata <tatarusetskaya@gmail.com>
2021-09-15 22:02:55 +0300

    Update task3.js

--- sprint02/task3.js
+++ sprint02/task3.js
@@ -1,6 +1,14 @@
-const maxInterv = (...num) => {
-    if (num.length === 1) return 0;
-    return num.reduce((acc, curr, index, arr) => {
-        return acc >= Math.abs(arr[index - 1] - curr) ? acc : Math.abs(arr[index - 1] - curr);
-    }, num[0]);
+// Find the maximum interval between two consecutive numbers. Numbers are entered as arguments
+// Example:
+// maxInterv(3, 5, 2, 7); //5
+// maxInterv(3, 5, 2, 7, 11, 0, -2); //11
+// maxInterv(3, 5); //2
+// maxInterv(3); //0
+
+const maxInterv = (...arg) => {
+    returnarg.reduce((acc, currentValue, index, array) => {
+        let interval = array[index - 1] - currentValue;
+        if (interval < 0)
+            interval = -interval;
+        return interval > acc ? interval : acc}, 0);
 };
