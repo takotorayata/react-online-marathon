7f61c8d915eea7538a265bd75f24160c567c1c54
Tata <tatarusetskaya@gmail.com>
2021-09-15 22:03:59 +0300

    Update task5.js

--- sprint02/task5.js
+++ sprint02/task5.js
@@ -1,1 +1,8 @@
+// The function takes any number of strings and returns the sum of their lengths.
+// Example:
+// console.log(sumOfLen('hello', 'hi')); //7
+// console.log(sumOfLen('hi')); //2
+// console.log(sumOfLen()); //0
+// console.log(sumOfLen('hello', 'hi', 'my name', 'is')); //16
+
 const combineFunctions = (...all) => x => all.reduce((a, b) => b(a), x);
