5ce5fc5e23c363f7d2de6bebb0decb017dc377a1
Tata <tatarusetskaya@gmail.com>
2021-09-15 21:51:55 +0300

    Update task2.js

--- sprint03/task2.js
+++ sprint03/task2.js
@@ -1,3 +1,7 @@
+// We have the product() function, you can see it on the snapshot below. This product() function finds the product of its arguments and also uses this object for the initial value of the product.
+// Please, create a new function getProduct()  that, no matter how it is called, will be always bound to a particular this value. getProduct() should be created from the original product() function and work with the same logic, but should pass two additional arguments – 2 and 3 – to the original function, every time getProduct() is called.
+// Object this for getProduct() function you should also define by yourself. Look at snapshot for clues what it should be.
+
 const product = function() {
     return [].reduce.call(arguments, function(res, elem) {
       return res * elem;
