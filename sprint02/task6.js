6fbf57d8514f4b68d7dbf9b32698ea05bf2af976
Tata <tatarusetskaya@gmail.com>
2021-09-15 22:05:36 +0300

    Update task6.js

--- sprint02/task6.js
+++ sprint02/task6.js
@@ -1,1 +1,28 @@
 const getLanguages = (...all) => all.reduce((a, b) => a.filter(b)).map((x) => x.languages).flat();
+
+// Suppose, you have an array of students:
+// let students = [{
+//   name: 'Anna',
+//   languages: ['English', 'Ukrainian'],
+//   age: 21
+// }, {
+//   name: 'Bob',
+//   languages: ['Polish', 'Spanish'], 
+//   age: 26 
+// }, { 
+//   name: 'Alice', 
+//   languages: ['Italian', 'Russian'], 
+//   age: 18 
+// }]
+// Please, implement a function getLanguages.
+// The function takes an array of students as a first parameter
+// and a condition on a student (function)
+// getLanguages should return an array of languages from students that satisfy a condition.
+// For example:
+// getLanguages(students, student => student.age < 26) should return
+// ['English', 'Ukrainian',  'Italian', 'Russian']
+// getLanguages(students, student => student.name === 'Alice') should return
+// ['Italian', 'Russian']
+// getLanguages(students) should return
+// ['English', 'Ukrainian','Polish', 'Spanish', 'Italian', 'Russian']
+// Try to use reduce and not use loops to solve this task. 
