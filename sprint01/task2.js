7bee5c1b5951f357546a172c6744f7c77eebf583
Tata <tatarusetskaya@gmail.com>
2021-09-15 22:07:18 +0300

    Update task2.js

--- sprint01/task2.js
+++ sprint01/task2.js
@@ -5,3 +5,7 @@ function combineArray(arr1, arr2) {
 
 //must be edited; must return Infinity too, not only +- integers;
     
+// Write a function combineArray(arr1, arr2), which takes 2 arrays, and returns a new array consisting only of numeric elements of arrays arr1 and arr2.
+// Function example:
+// combineArray([12, "User01", 22, true, -8], ["Index", 6, null, 15]));  // [12, 22, -8, 6, 15]
+
