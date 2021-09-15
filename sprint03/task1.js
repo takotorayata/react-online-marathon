a17c042a0cebdb9ba689539315da544723c99b65
Tata <tatarusetskaya@gmail.com>
2021-09-15 21:52:36 +0300

    Update task1.js

--- sprint03/task1.js
+++ sprint03/task1.js
@@ -1,3 +1,6 @@
+// Implement the getMin(arr) function, which takes an array of numbers arr and returns the smallest number of the array.
+// To solve the problem, you must use one of the methods to specify the context of this. It is forbidden to use any cycles.
+
 function getMin(arr) {
     return Math.min.apply(null, arr);
 }
