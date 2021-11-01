3514f6a575b69f6f3591f590429b2f0c5aa039ac
Tata <tatarusetskaya@gmail.com>
2021-11-01 18:57:29 +0200

    Create countIncrementsSeenMiddleware.js

---
+++ spr13/src/tasks/03-middleware-enhancers/countIncrementsSeenMiddleware.js
@@ -0,0 +1,20 @@
+const countIncrementsSeenMiddleware = (storeAPI, count = 0) => next => action => {
+    if (action.type === 'INCREMENT') {
+        next({
+            type: 'INCREMENTS_SEEN',
+            count: ++count,
+        });
+    }
+    return next(action);
+}
+
+export default countIncrementsSeenMiddleware;
+
+// TODO This middleware should keep an internal counter that tracks how many times it's seen
+// TODO the "INCREMENT" action.  Every time it sees one, it should send the increment action onwards, then
+// TODO increment the counter and dispatch an action like `{type : "INCREMENTS_SEEN", count}` after the
+// TODO increment action has returned.
+
+// TODO This means you'll need to keep a variable alive in the middleware between dispatches, so start
+// TODO by splitting up the triple-functions structure so you have a place to put the counter.
+
