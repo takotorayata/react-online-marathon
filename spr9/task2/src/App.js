eec2091c22c4ceac7613b4a01f4fd59e247150d7
Tata <tatarusetskaya@gmail.com>
2021-11-01 19:18:03 +0200

    Update App.js

--- spr9/task2/src/App.js
+++ spr9/task2/src/App.js
@@ -1,22 +1,22 @@
-There are two components - App and its children component First
+// There are two components - App and its children component First
 
-Write code to provide:
+// Write code to provide:
 
-Using the props the component App passes into the component First the list converted into lowercase:
-Animals
-Anime
-Anti-Malware
-Art Design
-Books
-Business
-Calendar
-Cloud Storage
-File Sharing
-Animals
-Continuous Integration
-Cryptocurrency
-The component First renders the first (is at index 0) element of the array received in props.
-Example
+// Using the props the component App passes into the component First the list converted into lowercase:
+// Animals
+// Anime
+// Anti-Malware
+// Art Design
+// Books
+// Business
+// Calendar
+// Cloud Storage
+// File Sharing
+// Animals
+// Continuous Integration
+// Cryptocurrency
+// The component First renders the first (is at index 0) element of the array received in props.
+// Example
 
 import React from "react";
 import "./App.css";
