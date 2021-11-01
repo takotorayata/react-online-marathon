3f0887c57fcd384de020b17725cb6beaa9a07110
Tata <tatarusetskaya@gmail.com>
2021-11-01 18:26:20 +0200

    Update and rename src/App.js to task1/src/App.js

--- task1/src/App.js
+++ task1/src/App.js
@@ -24,24 +24,24 @@ export default function App() {
   );
 }
 
-The tasks of the topic "Keys"
-There is the component App
+// The tasks of the topic "Keys"
+// There is the component App
 
-Write code to provide:
+// Write code to provide:
 
-Display in the component App the next list:
-Animals
-Anime
-Anti-Malware
-Art Design
-Books
-Business
-Calendar
-Cloud Storage
-File Sharing
-Animals
-Continuous Integration
-Cryptocurrency
+// Display in the component App the next list:
+// Animals
+// Anime
+// Anti-Malware
+// Art Design
+// Books
+// Business
+// Calendar
+// Cloud Storage
+// File Sharing
+// Animals
+// Continuous Integration
+// Cryptocurrency
 
-Use the element li to represent an item in a list
-Provide some unique keys to render this list, but don't use the index of an item as its key
+// Use the element li to represent an item in a list
+// Provide some unique keys to render this list, but don't use the index of an item as its key
