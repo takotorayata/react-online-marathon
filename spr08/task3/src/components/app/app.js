c4d905e75c7a9e7fa7126fdac34279fedc32db55
Tata <tatarusetskaya@gmail.com>
2021-11-01 18:19:15 +0200

    Create app.js

---
+++ spr08/task3/src/components/app/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '../button/button.js';
+import './app.css';
+
+export default function App(){
+    return <div className='wrapper-app' data-testid='element-app'><Button /></div>;
+}
