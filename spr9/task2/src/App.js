51630794949fd77d412c708c87da18419039db6c
Tata <tatarusetskaya@gmail.com>
2021-11-01 18:28:45 +0200

    Create App.js

---
+++ spr9/task2/src/App.js
@@ -0,0 +1,34 @@
+There are two components - App and its children component First
+
+Write code to provide:
+
+Using the props the component App passes into the component First the list converted into lowercase:
+Animals
+Anime
+Anti-Malware
+Art Design
+Books
+Business
+Calendar
+Cloud Storage
+File Sharing
+Animals
+Continuous Integration
+Cryptocurrency
+The component First renders the first (is at index 0) element of the array received in props.
+Example
+
+import React from "react";
+import "./App.css";
+import First from "./First";
+
+export default function App() {
+  const list = ["Animals", "Anime", "Anti-Malware", "Art Design", "Books", "Business", "Calendar", "Cloud Storage", 
+                "File Sharing", "Animals", "Continuous Integration", "Cryptocurrency"];
+  return (
+    <div>
+      Some data:
+        <First props={list.map(el => el.toLowerCase())}/>
+    </div>
+  );
+}
