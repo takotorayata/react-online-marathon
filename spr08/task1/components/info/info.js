6e449a3a0571a23f1a2279494a12da2967cd452a
Tata <tatarusetskaya@gmail.com>
2021-11-01 18:14:56 +0200

    Create info.js

---
+++ spr08/task1/components/info/info.js
@@ -0,0 +1,12 @@
+// Change file ./src/components/info/info.js.
+
+// Function Info must return JSX div element with:
+
+// text inside : JSX produces React 'elements'
+// attribute data-testid with value element-info
+
+import React from 'react';
+
+export default function Info(){
+    return <div data-testid='element-info'>JSX produces React 'elements'</div>;
+}
