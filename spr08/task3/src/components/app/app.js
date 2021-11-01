d5ccb83c797b9830dacd417c4cdd11189111d263
Tata <tatarusetskaya@gmail.com>
2021-11-01 18:20:33 +0200

    Update app.js

--- spr08/task3/src/components/app/app.js
+++ spr08/task3/src/components/app/app.js
@@ -5,3 +5,12 @@ import './app.css';
 export default function App(){
     return <div className='wrapper-app' data-testid='element-app'><Button /></div>;
 }
+
+// App component at ./components/app/app.js
+
+// import app.css as css-file
+// add html attr:class wrapper-app to div tag
+// Button component at ./components/button/button.js
+
+// add inner text Wake up Neo...
+// add inline styles to button tag with this values:
