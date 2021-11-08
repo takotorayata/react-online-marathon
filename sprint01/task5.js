6e5a9a270bca73053f70418176490387320949ef
Tata <tatarusetskaya@gmail.com>
2021-11-08 14:37:30 +0200

    Update task5.js

--- sprint01/task5.js
+++ sprint01/task5.js
@@ -17,7 +17,7 @@ function checkAdult(age) {
     try {
         if(age === '' || age === null || typeof(age) === 'undefined') throw new Error('Please, enter your age');
         if(age < 0) throw new Error('Please, enter positive number');
-        if(IsNaN(age)) throw new Error('Please, enter number');
+        if(isNaN(age)) throw new Error('Please, enter number');
         if(!Number.isInteger(age)) throw new Error('Please, enter integer number');
         if(age < 18) throw new Error('Acces denied - you're too young!');
         console.log('Acces allowed');
