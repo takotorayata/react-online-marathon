e93c11b6a0396e28ebd32e77761a8917b0738512
Tata <tatarusetskaya@gmail.com>
2021-11-08 14:35:33 +0200

    Update task4.js

--- sprint01/task4.js
+++ sprint01/task4.js
@@ -1,3 +1,11 @@
+// Implement the processArray(arr, factorial) function, which takes the first parameter of the array arr, and the second parameter the function factorial
+// and processes each element of the array arr with the function factorial, returning a new array (the source array arr does not change)
+// The function factorial(n) calculates and returns the factorial of the number n. For example factorial(4) returns 24.
+// Example
+// // determines the factorial of the number n
+// function factorial(n) { // your code}; 
+// processArray([1, 2, 3, 4, 5], factorial); // [1, 2, 6, 24, 120]
+
 function factorial(n) {
     return n ? n * factorial(n - 1);
 }
@@ -10,10 +18,4 @@ function processArray(arr, factorial) {
 //must be edited; it must be written without recursion
 
     
-// Implement the processArray(arr, factorial) function, which takes the first parameter of the array arr, and the second parameter the function factorial
-// and processes each element of the array arr with the function factorial, returning a new array (the source array arr does not change)
-// The function factorial(n) calculates and returns the factorial of the number n. For example factorial(4) returns 24.
-// Example
-// // determines the factorial of the number n
-// function factorial(n) { // your code}; 
-// processArray([1, 2, 3, 4, 5], factorial); // [1, 2, 6, 24, 120]
+
