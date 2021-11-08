655cdaf7386a850e517f2a5669d8fffd2a9bc464
Tata <tatarusetskaya@gmail.com>
2021-11-08 14:48:05 +0200

    Create task1.js

---
+++ sprint05/task1.js
@@ -0,0 +1,8 @@
+// Add two pairs to localStorage:
+// key: "login", value: "Tom12!"
+// key: "token", value: "QhuR56Rw"
+
+const localStorage = require("./localStorage.js");
+
+localStorage.setItem("login", "Tom12!");
+localStorage.setItem("token", "QhuR56Rw");
