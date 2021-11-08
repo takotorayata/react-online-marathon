09efe9b10004ebbbafc4bda8413110b9b8fb0d87
Tata <tatarusetskaya@gmail.com>
2021-11-08 14:33:47 +0200

    Update task1.js

--- sprint01/task1.js
+++ sprint01/task1.js
@@ -1,12 +1,12 @@
+// Implement the getModifiedArray(array) function, which takes an arbitrary array, and returns an array with the value of the first element of the array equal to “Start”, 
+// the last element of the array equal to “End” and the rest of elements should be the same as in an initial array.
+// The initial array should stay unchanged.
+// Function example:
+// getModifiedArray([12, 6, 22, 0, -8])); // [‘Start’, 6, 22, 0, ‘End’]
+
 function getModifiedArray(array) {
     const arr = [...array];
     arr[0] = 'Start';
     arr[arr.length - 1] = 'End';
     return arr;
 }
-
-// Implement the getModifiedArray(array) function, which takes an arbitrary array, and returns an array with the value of the first element of the array equal to “Start”, 
-// the last element of the array equal to “End” and the rest of elements should be the same as in an initial array.
-// The initial array should stay unchanged.
-// Function example:
-// getModifiedArray([12, 6, 22, 0, -8])); // [‘Start’, 6, 22, 0, ‘End’]
