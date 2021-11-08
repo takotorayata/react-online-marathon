8eee04c8f3ce01e455e47edb6fb4b5869237eba4
Tata <tatarusetskaya@gmail.com>
2021-11-08 14:54:32 +0200

    Update gulpfile.js

--- sprint06/task3/gulpfile.js
+++ sprint06/task3/gulpfile.js
@@ -13,3 +13,20 @@ const taskCSS = () =>
     .pipe(dest('output/'));
 
 exports.default = gulp.series(taskJS, taskCSS);
+
+
+// const gulp = require('gulp');
+// const {src, dest} = require('gulp');
+// const concat = require('gulp-concat');
+// async function taskJS() {
+//   return src('./src/**/*.js')
+//     .pipe(concat('result.js'))
+//     .pipe(dest('output/'));
+// }
+// async function taskCSS(cb) {
+//   return src('./src/**/*.css')
+//     .pipe(concat('result.css'))
+//     .pipe(dest('output/'));
+//   cb();
+// }
+// exports.default = gulp.series(taskJS, taskCSS);
