2fe8da13dfb876158035bc9dc9213142dad6cba1
Tata <tatarusetskaya@gmail.com>
2021-11-08 15:07:32 +0200

    Update and rename spr9/task3/src/App.js to spr09/task3/src/App.js

--- spr09/task3/src/App.js
+++ spr09/task3/src/App.js
@@ -1,11 +1,3 @@
-//The tasks of the topics "State" and "Events"
-//There is the component App
-
-//Write code to provide:
-
-//Using the property appData of state the component App renders one element div with text React Marathon in it
-
-//If you click mouse on this div element the component App will convert the value of property appData to lowercase
 
 import React from "react";
 import "./App.css";
